45ecf9c5cf2bbf0285b664e2eba6f690416ee92c
Jeremy Schoonover <skoontastic@gmail.com>
2021-03-16 16:31:52 -0500

    initial

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="flex flex-col items-center justify-center h-full mx-auto max-w-7xl">
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+
+</div>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="space-y-4 font-sans text-center">
+
+  <h1 class="text-5xl">{{ msg }}</h1>
+
+  <p>
+    <a class="text-green-600 hover:underline" href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
+    <a class="text-green-600 hover:underline" href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button class="px-2 py-1 border-2 border-green-600 rounded" @click="state.count++">count is: {{ state.count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</div>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
